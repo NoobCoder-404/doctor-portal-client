dcec9a8a7e1cb77a3a90177c4d1b6eed32af832d
Noob-H4cker <alsamiul123@gmail.com>
2022-12-29 12:54:39 +0600

    router set up basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
 
 const App = () => {
   return (
-    <div className="text-5xl">
-      <button
-        type="button"
-        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-        Pink to Orange
-      </button>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 };
