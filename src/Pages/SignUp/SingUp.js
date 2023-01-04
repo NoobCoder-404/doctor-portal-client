719e415da7d3a5198e48b9c1eb66d248c507d2f2
Noob-H4cker <alsamiul123@gmail.com>
2023-01-04 18:53:16 +0600

    form validation complete

--- src/Pages/SignUp/SingUp.js
+++ src/Pages/SignUp/SingUp.js
@@ -3,7 +3,11 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SingUp = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
   const handleRegister = (data) => {
     console.log(data);
   };
@@ -11,21 +15,32 @@ const SingUp = () => {
     <div className="flex justify-center mt-20 ">
       <div className="w-96 rounded-lg shadow-2xl p-6">
         <div>
-          <h3 className="text-center text-2xl font-bold my-3 ">Sign Up</h3>
+          <h3 className="text-3xl mb-6 text-center my-3 ">Sign Up</h3>
         </div>
         <form onSubmit={handleSubmit(handleRegister)}>
           <div className="form-control w-full mb-3">
             <label className="label">
               <span className="label-text">Full Name</span>
             </label>
-            <input type="text" {...register('fullName')} className="input input-bordered w-full " />
+            <input
+              type="text"
+              {...register('fullName', { required: 'Name is required' })}
+              className="input input-bordered w-full "
+            />
           </div>
 
           <div className="form-control w-full mb-3">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" {...register('email')} className="input input-bordered w-full " />
+            <input
+              type="email"
+              {...register('email', {
+                required: 'Email is required'
+              })}
+              className="input input-bordered w-full "
+            />
+            {errors.email && <p className="text-red-600">{errors.email?.message}</p>}
           </div>
 
           <div className="form-control w-full mb-3">
@@ -34,9 +49,20 @@ const SingUp = () => {
             </label>
             <input
               type="password"
-              {...register('password')}
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 6,
+                  message: 'password must be 6 character or long'
+                },
+                pattern: {
+                  value: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+                  message: 'password must be strong'
+                }
+              })}
               className="input input-bordered w-full "
             />
+            {errors.password && <p className="text-red-600">{errors.password?.message}</p>}
             <label className="label">
               <span className="label-text">Forgot Password?</span>
             </label>
